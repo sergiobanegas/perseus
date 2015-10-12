476151c99498e90828c9a243919763dadab6a8be
sergiobanegas <sbanegas92@gmail.com>
2015-10-12 18:48:02 +0200

    Rooms and teams added

--- src/main/resources/static/angular/services/serviceUser.js
+++ src/main/resources/static/angular/services/serviceUser.js
@@ -50,7 +50,7 @@ function serviceUser($resource, $timeout, $cookieStore) {
 	function getUser(id) {
 		for (var i = 0; i < users.length; i++) {
 			if (users[i].id.toString() === id.toString()) {
-				
+				return users[i];
 			}
 		}
 	};
