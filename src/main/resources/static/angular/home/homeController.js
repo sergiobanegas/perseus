b2295d6e66831d5c87be851aa5b01178c43c46cb
Banegas <sbanegas92@gmail.com>
2016-01-28 16:31:22 +0100

    Material design visual updates

--- src/main/resources/static/angular/home/homeController.js
+++ src/main/resources/static/angular/home/homeController.js
@@ -3,49 +3,252 @@
  * @author Sergio Banegas Cortijo
  */
 
-kurento_room.controller('homeController', function ($http, $resource, $scope, $window, serviceUser, serviceParticipate, serviceTeam, $location, $route, $filter, LxNotificationService, LxDialogService) {
-	
+kurento_room.controller('homeController', function ($mdDialog, $mdMedia, $scope, $http, $resource, $window, serviceUser, serviceTeam, serviceParticipate, $location, $route, $filter, serviceRequestJoinRoom) {
+
+	$scope.borrar=serviceRequestJoinRoom.getRequestJoinRooms();
 	$scope.user = serviceUser.getSession();
 	$scope.users = serviceUser.getUsers();
 	$scope.teams= serviceParticipate.getParticipates();
 	$scope.teams2 = serviceTeam.getTeams();
 	$scope.primerUser=serviceUser.getUser(1);
-	$scope.userName = "";
-	
 	$scope.teamsUser=[];	
+	$scope.teamName="";		
 	
-	$scope.teamName="";
+	$scope.fileUpload;
 	
-	$scope.password = "";	
-	
-	$scope.login = function() {
-			if ($scope.userName && $scope.password){
-				var array= $filter('filter')($scope.users, { name: $scope.userName, password: $scope.password });
-				if (array.length!=0){
-					serviceUser.loginUser(array[0]);
-					$scope.user=serviceUser.getSession();
-					$window.location.reload();
-					LxNotificationService.success("Welcome "+$scope.user.name+"!");
-				}
-				else{
-					LxNotificationService.error('The user name or the password are incorrect');	
-				}
-			}
-			else{
-				LxNotificationService.error('Fill in all fields');
-			}
-	};
+	$scope.uploadImage = function(){
+		var formData = new FormData();
+		formData.append("file", $scope.fileUpload);
+		$http.post("/uploaduserimage", formData, {
+			transformRequest: angular.identity,
+			headers: {'Content-Type': undefined}
+		});
+
+	}
 	
 	$scope.logout = function(){		
 		serviceUser.logout();
 		user={};
 		$route.reload();
-		LxNotificationService.success("Goodbye!");
 	};
 	
+	$scope.exit = function(){
+		$window.location.href = '#/';
+	};
 	
-	$scope.opendDialog = function(dialogId){
-	    LxDialogService.open(dialogId);
+	$scope.login = function($event) {
+	    var parentEl = angular.element(document.body);
+	    $mdDialog.show({
+	      parent: parentEl,
+	      targetEvent: $event,
+	      template:
+	        '<md-dialog aria-label="List dialog" ng-cloak flex="50">' +
+	        '<md-toolbar>'+
+	        '<div class="md-toolbar-tools">'+
+	         ' <h2>Login</h2>'+
+	          '<span flex></span>'+
+	          '<md-button class="md-icon-button" ng-click="cancel()">'+
+	           ' <md-icon md-svg-src="img/icons/ic_close_24px.svg" aria-label="Close dialog"></md-icon>'+
+	          '</md-button>'+
+	        '</div>'+
+	        '</md-toolbar>'+
+	        '<md-dialog-content>'+
+	        '<div class="md-dialog-content">'+
+	        '<div layout="column" ng-cloak class="md-inline-form">'+
+	        '<md-input-container class="md-icon-float md-block">'+
+	        '<label>Username</label>'+
+	        '<md-icon aria-label="Name" class="material-icons" style="color: blue;">person</md-icon>'+
+	        '<input ng-model="userName">'+
+	        '</md-input-container>'+
+	        '<md-input-container>'+
+	        '<label>Password</label>'+
+	        '<md-icon aria-label="vpn_key" class="material-icons" style="color: blue;">vpn_key</md-icon>'+
+	        '<input type="password" ng-model="password">'+
+	        '</md-input-container>'+
+	        '<div>'+
+	        'Are you new?'+
+	        '<md-button style="background-color:purple" ng-click="goRegistration()">Register</md-button>'+
+	        '</div>'+
+	        '</div>'+
+	        '</div>'+
+	        '  </md-dialog-content>' +
+	        '  <md-dialog-actions>' +
+	        '  <md-button style="background-color:greenyellow" ng-click="login()" >' +
+	        '      Login' +
+	        '    </md-button>' +
+	        '    <md-button ng-click="closeDialog()" class="md-primary">' +
+	        '      Cancel' +
+	        '    </md-button>' +
+	        '  </md-dialog-actions>' +
+	        '</md-dialog>',
+	      locals: {
+	      },
+	      controller: DialogController
+	   })
+	};
+	$scope.joinTeam = function($event){
+		var parentEl = angular.element(document.body);
+	    $mdDialog.show({
+	      parent: parentEl,
+	      targetEvent: $event,
+	      template:
+	        '<md-dialog aria-label="List dialog" style="height:100%;width:100%"ng-cloak>' +
+	        '<md-toolbar>'+
+	        '<div class="md-toolbar-tools">'+
+	          '<span flex><h2>Join a team</h2></span>'+
+	          '<md-button class="md-icon-button" ng-click="closeDialog()">'+
+	           ' <md-icon class="material-icons" aria-label="Close dialog">close</md-icon>'+
+	          '</md-button>'+
+	        '</div>'+
+	        '</md-toolbar>'+
+	        '<md-dialog-content>'+
+	        '<div class="md-dialog-content">'+
+	        'Join a team</br>'+
+	        'Insert the name of the team and the password admins: {{admins}}'+
+	        '<div>'+
+	        '<md-input-container>'+
+	        '<label>Team name</label>'+
+	        '<input ng-model="team.name">'+
+	        '</md-input-container>'+
+	      	'<md-input-container>'+
+	      	'<label>Password</label>'+
+	      	'<input ng-model="team.password" type="password">'+
+	      	'</md-input-container>'+
+	        '<md-button class="md-primary" ng-click="joinTeam(team)">'+
+	        'Join'+
+	        '</md-button>'+
+	        '</div>'+
+	        "If you don't know the team password, you can send a petition to the team:"+
+	        '<div>'+
+	        '<md-input-container>'+
+	        '<label>Team name</label>'+
+	        '<input ng-model="teamRequested">'+
+	        '</md-input-container>'+
+	      	'<md-input-container>'+
+	      	'<md-button class="md-primary" ng-click="teamJoinRequest(teamRequested)">'+
+	        'Send petition'+
+	        '</md-button>'+
+	      	'</div>'+
+	        '</div>'+
+	        '</md-dialog>',
+	      locals: {
+	      },
+	      controller: DialogController
+	   })
 	};
 	
 });
+function DialogController($scope, $http, $mdDialog, $mdToast, $filter, $window, serviceUser, serviceTeam, serviceParticipate, serviceRequestJoinTeam) {
+	   $scope.user = serviceUser.getSession();
+	   $scope.users = serviceUser.getUsers();
+	   $scope.userName = "";
+	   $scope.password = "";	
+		
+		$scope.login = function() {
+			if ($scope.userName && $scope.password){
+				var array= $filter('filter')($scope.users, { name: $scope.userName, password: $scope.password });
+				if (array.length!=0){
+					$mdDialog.hide();
+					serviceUser.loginUser(array[0]);
+					$scope.user=serviceUser.getSession();
+					$window.location.reload();
+				}
+				else{
+					$scope.notification("The username or password are incorrect");	
+				}
+			}
+			else{
+					$scope.notification("Please fill both fields");	
+			}
+		};
+		
+		$scope.joinTeam = function(Team) {
+			if ( $filter('filter')(serviceTeam.getTeams(), { name: Team.name, password: Team.password}).length!=0){	
+				var TeamJoined= ($filter('filter')(serviceTeam.getTeams(), { name: Team.name}))[0];
+					if ( $filter('filter')(serviceParticipate.getParticipates(), { iduser: $scope.user.id, idteam: TeamJoined.id }).length==0 ){			
+						var newParticipate={};
+						newParticipate.iduser=$scope.user.id;
+						newParticipate.idteam=TeamJoined.id;
+						newParticipate.userName=$scope.user.name;
+						newParticipate.teamName=Team.name;
+						newParticipate.teamPrivileges=0;
+						serviceParticipate.newParticipate(newParticipate);	
+						$mdDialog.hide();
+						$scope.notification("You succesfully joined "+TeamJoined.name+"!");	
+					}
+					else{
+						$scope.notification("You've already registered in the team "+Team.name);
+					}
+			}
+			else{
+				$scope.notification("The name of the team or the password of the team are incorrect!");
+			}
+		};
+		
+		$scope.findUserById = function(iduser){
+			for (var i=0; i< serviceUser.getUsers().length;i++){
+				if (serviceUser.getUsers()[i].id==iduser){
+					return serviceUser.getUsers()[i];
+					break;
+				}
+			}
+		}
+		
+		$scope.teamJoinRequest = function(name){		
+				var team={};
+				for (var i=0;i<serviceTeam.getTeams().length;i++){
+					if (serviceTeam.getTeams()[i].name==name){
+						team=serviceTeam.getTeams()[i];
+						break;
+					}
+				}
+				if (team){
+					if ($filter('filter')(serviceParticipate.getParticipates(), { iduser: $scope.user.id, idteam: team.id }).length==0 ){			
+						if  ($filter('filter')(serviceRequestJoinTeam.getRequestJoinTeams(), { user: $scope.user.id, team: team.id}).length==0){							
+							var newRequest={};
+							newRequest.user=$scope.user.id;
+							newRequest.team=team.id;
+							serviceRequestJoinTeam.newRequestJoinTeam(newRequest);
+//							var admins=serviceParticipate.getModerators(team.name);
+							for (var i=0;i<serviceParticipate.getParticipates().length;i++){
+								if (serviceParticipate.getParticipates()[i].idteam=team.id && serviceParticipate.getParticipates()[i].teamPrivileges>0){
+										var data= {
+												"user": $scope.user, 
+												"team": team,
+												"admin": $scope.findUserById(serviceParticipate.getParticipates()[i].iduser)
+												};
+										$http.post("/sendrequestjointeam", data);
+								}
+								$mdDialog.hide();
+								$scope.notification("A petition was sent to the team "+team.name);
+								return 0;
+							}		
+						}else{
+							$scope.notification("You've already sent a request to enter "+team);
+						}	
+					}else{
+						$scope.notification("You already are a member of the team "+teamObject.name);
+					}	
+			}else{
+				$scope.notification("That team doesn't exists");
+			}
+		}
+	   
+	   $scope.goRegistration = function (){
+		   $mdDialog.hide();
+		   $window.location.href = '#/registration';  
+	   };
+	   
+	   $scope.notification = function(text) {
+		    $mdToast.show(
+		      $mdToast.simple()
+		        .textContent(text)
+		        .position("bottom right")
+		        .hideDelay(3000)
+		    );
+		};
+	   
+     $scope.closeDialog = function() {
+       $mdDialog.hide();
+     }
+}
