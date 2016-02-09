062544577e884949865bddf793a8155f08aa3e51
Sergio Banegas <sbanegas92@gmail.com>
2016-02-09 22:14:24 +0100

    Notification delay modified

--- src/main/resources/static/angular/services/serviceNotification.js
+++ src/main/resources/static/angular/services/serviceNotification.js
@@ -16,7 +16,7 @@ function serviceNotification(webNotification) {
             onClick: function onNotificationClicked() {
                 window.alert('Notification clicked.');
             },
-            autoClose: 4000 //auto close the notification after 2 seconds (you manually close it via hide function)
+            autoClose: 500 //auto close the notification after 2 seconds (you manually close it via hide function)
         }, function onShow(error, hide) {
             if (error) {
                 window.alert('Unable to show notification: ' + error.message);
@@ -26,7 +26,7 @@ function serviceNotification(webNotification) {
                 setTimeout(function hideNotification() {
                     console.log('Hiding notification....');
                     hide(); //manually close the notification (or let the autoClose close it)
-                }, 5000);
+                }, 500);
             }
         });
 	}
