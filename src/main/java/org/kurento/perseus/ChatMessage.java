270f97421fa3753f72bcae16bdbd3492fd3f127f
sergiobanegas <sbanegas92@gmail.com>
2016-02-23 17:11:43 +0100

    Warnings removed

--- src/main/java/org/kurento/perseus/ChatMessage.java
+++ src/main/java/org/kurento/perseus/ChatMessage.java
@@ -1,12 +1,6 @@
 package org.kurento.perseus;
 
 
-
-
-
-
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
